90fe9fc8d449dd3345c449aea7ac99a22976eda9
OTHMANE ELGOUBI <othmanelgoubi@gmail.com>
2021-05-01 14:51:23 +0200

    simple usage of docker-compose

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/",(req,res,next)=>{
-    res.send("Hello Lovely Docker ! Let's play with it now");
+    res.send("Hello Lovely Docker ! Let's play with it now using docker-compose");
 })
 
 app.get("/hey",(req,res,next)=>{
-    res.send("Hello From Docker container");
+    res.send("Hello From Docker container using Docker compose");
 })
 
 
