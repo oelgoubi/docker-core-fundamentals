7cae1f1b999e9ee62e866a1d3a156bc1c2a23eb0
OTHMANE ELGOUBI <othmanelgoubi@gmail.com>
2021-05-01 14:33:29 +0200

    how to pass env variables to containers

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/",(req,res,next)=>{
-    res.send("Hello Lovely Docker ! Let's play with it");
+    res.send("Hello Lovely Docker ! Let's play with it now");
 })
 
 app.get("/hey",(req,res,next)=>{
